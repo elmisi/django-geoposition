e0cc5a47ef109dc13c3a9a9685596fa2a8092819
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-03-11 11:34:08 +0100

    Update metadata

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     version=__import__('geoposition').__version__,
     description=
     'Django model field that can hold a geoposition, and corresponding admin widget.',
-    author='Dario Castañé',
-    author_email='i@dario.im',
-    url='http://github.com/imdario/django-geoposition',
+    author='Michael Verdegaal',
+    author_email='michaelverdegaal@hotmail.nl',
+    url='https://github.com/MichaelVerdegaal/django-geoposition',
     packages=find_packages(),
     zip_safe=False,
     package_data={
@@ -30,5 +30,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Django',
     ])
